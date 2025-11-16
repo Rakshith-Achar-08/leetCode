ce2dd54187ed913e9e55f1b5e6a6557a5c0cb033
Rakshith-Achar-08 <rakshithachar08@gmail.com>
2025-11-16 22:59:22 +0530

    .

--- LeetSolver/README.md
+++ LeetSolver/README.md
@@ -4,3 +4,5 @@
 2. `ListNode`: Important class used in [AddTwoNumber].
 3. `chatAt(i);`: [RomanConverter],[ValidParanthesis].
 4. `toCharArray();`: [ValidParenthesis].
+5. `containsKey(abcd);`: [ValidParenthesis].
+6. `containsValues(abcd);`: [ValidParenthesis]
--- LeetSolver/src/ValidParanthesis/ValidParanthesis.java
+++ LeetSolver/src/ValidParanthesis/ValidParanthesis.java
@@ -6,11 +6,59 @@ class Solution{
 
     public boolean isValid(String str){
 
+        // Using a stack to hold characters of String value;
+        Stack<Character> stack = new Stack<>();
+
+        // By using a map. It is represents a collection of key-value pairs,
+        // where Keys should be unique, but values can be duplicated.
+        Map<Character, Character> map= new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        for(char paran : str.toCharArray()){
+
+            //containsKey() method is used to check whether a particular key is being mapped into the HashMap or not.
+            // It takes the key element as a parameter and returns True if that element is mapped in the map.
+            // the containsKey() method is boolean.
+
+            //if this statement becomes flase, then the character will directly be pushed to stack --v
+            if(map.containsKey(paran)){
+
+                //Suppose if condition becomes true (like when the loop encounter ')','}', ']' it is satisfied)
+
+                char topElement = stack.isEmpty() ? '#' : stack.pop();    // <--topElement variale stores the top element by poping.
+
+
+                if(topElement != map.get(paran)){  // <--this statement is used whether the topElement and current char is same,
+                    return false; //<--if it is true, then true, orelse false.
+                }
+            }else{
+                // ^-- here
+                stack.push(paran);
+            }
+        }
+        return stack.isEmpty(); //<-- return empty stack is actually an answer for this problem.
     }
 }
 
 public class ValidParanthesis {
     static void main() {
+
+        String str1 = "{}";
+        String str2 = "[][}";
+        Solution solution = new Solution();
+        if(solution.isValid(str1)){
+            System.out.println("The given parenthesis is valid");
+        }else{
+            System.out.println("The given parenthesis is not valid");
+        }
+        if(solution.isValid(str2)){
+            System.out.println("The given parenthesis is valid");
+        }else{
+            System.out.println("The given parenthesis is not valid");
+        }
+
 
     }
 }
